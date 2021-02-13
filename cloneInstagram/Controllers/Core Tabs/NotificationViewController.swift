dc2eb247afd890a35a055c21c2d030a01e0e9755
SemaDevpy <syimykdev@gmail.com>
2021-02-13 09:54:04 +0600

    add backbone UI for all main pages.

--- cloneInstagram/Controllers/Core Tabs/NotificationViewController.swift
+++ cloneInstagram/Controllers/Core Tabs/NotificationViewController.swift
@@ -8,23 +8,47 @@
 
 import UIKit
 
-class NotificationViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+class NotificationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
     
 
-    /*
-    // MARK: - Navigation
+    private let tableView : UITableView = {
+       let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+       return tableView
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        title = "Notififcations"
+        view.backgroundColor = .systemBackground
+        view.addSubview(tableView)
     }
-    */
 
+
+    override func viewDidLayoutSubviews() {
+        tableView.frame = view.bounds
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("something!")
+    }
+    
 }
--- cloneInstagram/Controllers/Core Tabs/ProfileViewController.swift
+++ cloneInstagram/Controllers/Core Tabs/ProfileViewController.swift
@@ -9,16 +9,38 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    private var collectionView : UICollectionView?
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top:  0, left: 0, bottom: 0, right: 0)
+        layout.itemSize = CGSize(width: view.width/3, height: view.width/3)
+        collectionView?.delegate = self
+        collectionView?.dataSource =  self
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
         view.backgroundColor = .systemBackground
         configureNavBar()
+        guard let collectionView = collectionView else{
+            return
+        }
+        view.addSubview(collectionView)
     }
 
     private func configureNavBar(){
         navigationItem.rightBarButtonItem = UIBarButtonItem(image:  UIImage(systemName: "gear"), style: .done, target: self, action: #selector(didTapSettingsButton))
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView?.frame = view.bounds
     }
     
     
@@ -30,4 +52,18 @@ class ProfileViewController: UIViewController {
     
     
     
+}
+extension ProfileViewController : UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("IndexPath ")
+    }
+        
 }
